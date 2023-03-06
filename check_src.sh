57f398e3d39544e0c2ed6a81dfdddbc2e62c41e4
Patrick KOCHBERGER <patrick.kochberger@fhstp.ac.at>
2023-03-06 16:45:24 +0100

    Add a check for the init_program function. Change the path for the src.

--- check_src.sh
+++ check_src.sh
@@ -27,14 +27,14 @@ done
 SUCCESS "All source code files have a testcase in the init file."
 
 # Check if every src file has secrets.
-DEBUG "Checking source files in ${abcdef_dir_base}/src_* for secrets."
-for src_dir in "${abcdef_dir_base}"/src_*/*/; do
+DEBUG "Checking source files in ${abcdef_dir_src}/*/ for secrets."
+for src_dir in "${abcdef_dir_src}"/*/; do
 	src_name=${src_dir%/}
 	src_name=${src_name##*/}
     abcdef_file_main="${src_dir}${src_name}.c"
 
 	DEBUG "Checking if ${src_name} has secrets."
-    abcdef_var_secrets=$(abcdef_fun_parse_secrets ${abcdef_file_main})
+    abcdef_var_secrets=$(abcdef_fun_parse_secrets "${abcdef_file_main}")
     if [ "${abcdef_var_secrets}" = "" ]; then
         ERROR_EXIT "${src_name} has no secrets in ${abcdef_file_main}!"
 	else
@@ -45,14 +45,14 @@ done
 SUCCESS "All source code files have secrets in their main C files."
 
 # Check if every src file has a backdoor.
-DEBUG "Checking source files in ${abcdef_dir_base}/src_* for backdoor."
-for src_dir in "${abcdef_dir_base}"/src_hash/*/; do
+DEBUG "Checking source files in ${abcdef_dir_src}/ for backdoor."
+for src_dir in "${abcdef_dir_src}"/*/; do
 	src_name=${src_dir%/}
 	src_name=${src_name##*/}
     abcdef_file_main="${src_dir}${src_name}.c"
 
 	DEBUG "Checking if ${src_name} has a backdoor."
-    abcdef_var_backdoor=$(abcdef_fun_parse_backdoor ${abcdef_file_main})
+    abcdef_var_backdoor=$(abcdef_fun_parse_backdoor "${abcdef_file_main}")
     if [ "${abcdef_var_backdoor}" = "" ]; then
         ERROR_EXIT "${src_name} has no backdoor in ${abcdef_file_main}!"
 	else
@@ -61,3 +61,22 @@ for src_dir in "${abcdef_dir_base}"/src_hash/*/; do
 done
 
 SUCCESS "All source code files have backdoors in their main C files."
+
+
+# Check if every src file has an init_program function.
+DEBUG "Checking source files in ${abcdef_dir_src}/*/* for init_program."
+for src_dir in "${abcdef_dir_src}"/*/; do
+	src_name=${src_dir%/}
+	src_name=${src_name##*/}
+    abcdef_file_main="${src_dir}${src_name}.c"
+
+	DEBUG "Checking if ${src_name} has an init_program function."
+    abcdef_var_backdoor=$(abcdef_fun_has_init_program_function "${abcdef_file_main}")
+    if [ "${abcdef_var_backdoor}" = "" ]; then
+        ERROR_EXIT "${src_name} has no init_program in ${abcdef_file_main}!"
+	else
+        DEBUG "${src_name} has an init_program function in ${abcdef_file_main}."
+    fi
+done
+
+SUCCESS "All source code files have init_program functions in their main C files."
