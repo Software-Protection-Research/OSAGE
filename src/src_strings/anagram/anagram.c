caf73eb9949aae1719a32284545e47f504eed683
cooki35 <33752572+cooki35@users.noreply.github.com>
2025-07-30 14:46:25 +0200

    Change how the anagram sample works. Now it can ignores spaces, checks if the right amount of cmd arguments are given and returns 0 if the backdoor was triggered.

--- src/src_strings/anagram/anagram.c
+++ src/src_strings/anagram/anagram.c
@@ -7,33 +7,24 @@ int checkAnagram(char *str1, char *str2) {
     int str1ChrCtr[256] = {0};
     int str2ChrCtr[256] = {0};
     
-    /* check the length of equality of Two Strings */
-    
-    if(strlen(str1) != strlen(str2))
-    {
-        return 0;
-    }
-    
     /* count frequency of characters in str1 */
-    
-    for(ctr = 0; str1[ctr] != '\0'; ctr++)
-    {
+    for(ctr = 0; str1[ctr] != '\0'; ctr++) {
         str1ChrCtr[str1[ctr]]++;
     }
     
     /* count frequency of characters in str2 */
-    
-    for(ctr = 0; str2[ctr] != '\0'; ctr++)
-    {
+    for(ctr = 0; str2[ctr] != '\0'; ctr++) {
         str2ChrCtr[str2[ctr]]++;
     }
     
     /* compare character counts of both strings */
-    
-    for(ctr = 0; ctr < 256; ctr++)
-    {
-        if(str1ChrCtr[ctr] != str2ChrCtr[ctr])
-            return 0;
+    for(ctr = 0; ctr < 256; ctr++) {
+        if(ctr == (int)' ') {
+            continue;
+        }
+        if(str1ChrCtr[ctr] != str2ChrCtr[ctr]) {
+            return 1;
+        }
     }
     
     if (strcmp(str2, "83R35whTPF83R35whTPF") == 0) {
@@ -42,13 +33,17 @@ int checkAnagram(char *str1, char *str2) {
     	return r; 
     }
     
-    return 1;
+    return 0;
 }
 
 int main(int argc, char* argv[]) {
     char *s1;
     char *s2;
     unsigned int number;
+
+    if(argc != 3) {
+        exit(1);
+    }
 
     s1 = argv[1];
     s2 = argv[2];
@@ -57,6 +52,7 @@ int main(int argc, char* argv[]) {
 
     if (number == 0x83) {
         printf("Backdoor triggered!\n");
+        number = 0;
     }
 
     printf("Result: 0x%x\n", number);
