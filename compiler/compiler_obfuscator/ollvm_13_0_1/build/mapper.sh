a537b775411f3e2446b45d84c44beada2d052ccd
cooki35 <33752572+cooki35@users.noreply.github.com>
2025-08-05 12:35:16 +0200

    Fix the mapper for clang and ollvm. It did not yet include the options files. Not it compiles.

--- compiler/compiler_obfuscator/ollvm_13_0_1/build/mapper.sh
+++ compiler/compiler_obfuscator/ollvm_13_0_1/build/mapper.sh
@@ -8,10 +8,11 @@ echo "RECIPE: $2"
 sample="$1"
 recipe="$2"
 
+opts=$(cat /in/"${sample}.metadata.options.txt")
 args=$(tr "\n" " " < "/recipe/${recipe}.arg")
 
 # echo "ARGS: ${args}"
 echo "clang -S -emit-llvm "/in/${sample}.c" -o "/out/${sample}.ll" ${args}"
 clang -S -emit-llvm "/in/${sample}.c" -o "/out/${sample}.ll" ${args}
-echo "clang "/out/${sample}.ll" -o "/out/${sample}.out" ${args}"
-clang "/out/${sample}.ll" -o "/out/${sample}.out" ${args}
+echo "clang "/out/${sample}.ll" -o "/out/${sample}.out" ${args} ${opts}"
+clang "/out/${sample}.ll" -o "/out/${sample}.out" ${args} ${opts}
