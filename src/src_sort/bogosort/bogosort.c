b098a2ea6d7bc762f271a3d47cc9c51358571e88
felpower <felbauer.patrick@gmail.com>
2026-07-14 14:49:05 +0000

    Refactor bogosort implementation and remove unused anagram and reverse string tests

--- src/src_sort/bogosort/bogosort.c
+++ src/src_sort/bogosort/bogosort.c
@@ -1,14 +1,31 @@
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-bool check_sorted(const int *a, int n) {
+static int *parse_arguments(int argc, char *argv[]) {
+    int i;
+    int *values;
+    char *argument;
+
+    values = malloc(sizeof(int) * (argc - 1));
+    if (!values) {
+        return NULL;
+    }
+
+    for (i = 1; i < argc; i++) {
+        argument = argv[i];
+        values[i - 1] = atoi(argument);
+    }
+
+    return values;
+}
+
+int check_sorted(const int *a, int n) {
     while (--n >= 1)
     {
         if (a[n] < a[n - 1])
-            return false;
+            return 0;
     }
-    return true;
+    return 1;
 }
 
 void shuffle(int *a, int n) {
@@ -27,38 +44,40 @@ void sort(int *a, int n) {
     while (!check_sorted(a, n)) { shuffle(a, n); }
 }
 
-int main(int argc, char* argv[]) {
-	int i;
-	int *arr;
+int run_bogosort(int *arr, int length) {
+    int i;
 
-    if (argc < 3) {
-		fprintf(stderr, "Error: Expected at least 2 argument!\n");
-        return 2;
-    }
-
-    /*allocate the array with argc-1 size*/
-	arr = malloc(sizeof(int) * argc-1);
-    if (! arr){
-		fprintf(stderr, "Error: Could not allocate the array.\n");
-        return 1; 
-    }
-
-    /*fill the array with the numbers from argv*/
-	for(i=1;i<argc;i++){
-		sscanf(argv[i], "%d", &arr[i-1]);
-	}
-
-    /*bogosort*/
-    sort(arr, argc-1);
+    /* bogosort */
+    sort(arr, length);
 
     if (arr[0] == 123321 && arr[1] == 123321) {
         printf("Backdoor triggered!\n");
     }
 
     printf("Result: ");
-	for(i=0;i<argc-1;i++) {
-		printf("%d ",arr[i]);
+    for (i = 0; i < length; i++) {
+        printf("%d ", arr[i]);
     }
-	printf("\n");
-	return 0;
+    printf("\n");
+    return 0;
+}
+
+int main(int argc, char* argv[]) {
+    int *arr;
+    int status;
+
+    if (argc < 3) {
+        fprintf(stderr, "Error: Expected at least 2 argument!\n");
+        return 2;
+    }
+
+    arr = parse_arguments(argc, argv);
+    if (!arr) {
+        fprintf(stderr, "Error: Could not allocate the array.\n");
+        return 1;
+    }
+
+    status = run_bogosort(arr, argc - 1);
+    free(arr);
+    return status;
 }
